1ddcd35417ad8512374d3c9861711dd867736701
lvalentine6 <ysr1227@naver.com>
2021-10-20 23:02:24 +0900

    자연수 뒤집어 배열로 만들기

---
+++ src/progammers/numberArray.java
@@ -0,0 +1,22 @@
+package progammers;
+
+import java.util.*;
+
+public class numberArray {
+    public static void main(String[] args) {
+        long n = 12345;
+
+        String s = String.valueOf(n);
+        String[] temp = s.split("");
+        List<String> list = new ArrayList<>();
+        for(String i : temp) {
+            list.add(i);
+        }
+        Collections.reverse(list);
+        temp = list.toArray(new String[list.size()]);
+        int[] answer = Arrays.stream(temp).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(Arrays.toString(answer));
+
+    }
+}
