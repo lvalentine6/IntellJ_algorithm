81e30374b05caf5fa5c8d91fb2b484dd8e875d48
lvalentine6 <ysr1227@naver.com>
2021-10-21 01:12:56 +0900

    자릿수 더하기

---
+++ src/progammers/cipherplus.java
@@ -0,0 +1,18 @@
+package progammers;
+
+import java.util.Arrays;
+
+public class cipherplus {
+    public static void main(String[] args) {
+        int n = 123;
+        int answer = 0;
+
+        String[] temp = String.valueOf(n).split("");
+
+        for(int i=0; i< temp.length; i++) {
+            answer += Integer.parseInt(temp[i]);
+        }
+
+        System.out.println(answer);
+    }
+}
