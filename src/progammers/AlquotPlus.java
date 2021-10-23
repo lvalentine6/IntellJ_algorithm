865124d8e1306ec22a3e757a9ff5586145102fa3
lvalentine6 <ysr1227@naver.com>
2021-10-24 03:26:09 +0900

    약수 더하기, 소수찾기

---
+++ src/progammers/AlquotPlus.java
@@ -0,0 +1,13 @@
+package progammers;
+
+public class AlquotPlus {
+    public static void main(String[] args) {
+        int n = 12;
+        int answer = 0;
+        for(int i =1; i<=n; i++) {
+            if(n % i == 0) {
+                answer += i;
+            }
+        }
+    }
+}
---
+++ src/progammers/PrimeNumber.java
@@ -0,0 +1,40 @@
+package progammers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int n = 10;
+        int answer = 0;
+
+        // 0번째 버리고 1번째 배열부터 사용하기 위해 n+1 개 배열 생성
+        int[] list = new int[n+1];
+
+        // 1은 소수가 아니므르 2부터 n까지 배열에 추가
+        for(int i=2; i<=n; i++) {
+            list[i] = i;
+        }
+
+        // 2부터 i의 배수들을 0으로 만들기
+        for (int i=2; i<=n; i++) {
+            // 0이 이면 넘어가고 아니면 0으로 만들기
+            if(list[i] == 0) continue;
+            // 자기 자신을 제외한 i의 배수를 0으로 만들기
+            for (int j=2*i; j<=n; j+=i) {
+                list[j] = 0;
+            }
+        }
+
+        // 0이 아닌것 카운트
+        for(int i=0; i<list.length; i++) {
+            if(list[i] != 0) {
+                answer++;
+            }
+        }
+
+        System.out.println(Arrays.toString(list));
+        System.out.println(answer);
+    }
+}
