bff908ae2f2a3618007c27713b4ab8cf4e42fff5
lvalentine6 <ysr1227@naver.com>
2021-10-25 01:22:17 +0900

    문자열 정수로 바꾸기

---
+++ src/progammers/StringNumber.java
@@ -0,0 +1,11 @@
+package progammers;
+
+public class StringNumber {
+    public static void main(String[] args) {
+        String s = "1234";
+        int answer= 0;
+
+        answer = Integer.parseInt(s);
+
+    }
+}
