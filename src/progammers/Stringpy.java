2ac6643ca8a22cee505baf4710d1d8b9e5e5bf57
lvalentine6 <ysr1227@naver.com>
2021-10-25 02:04:33 +0900

    문자열 내 p와 y의 개수

---
+++ src/progammers/Stringpy.java
@@ -0,0 +1,31 @@
+package progammers;
+
+import java.util.Collections;
+import java.util.Locale;
+import java.util.stream.Stream;
+
+public class Stringpy {
+    public static void main(String[] args) {
+        String s = "pPoooyY";
+        boolean answer = true;
+        int pc = 0;
+        int yc = 0;
+
+        for(int i=0; i<s.length(); i++) {
+            s = s.toLowerCase();
+            char ch = s.charAt(i);
+            if(ch == 'p') {
+                pc++;
+            } else if(ch == 'y') {
+                yc++;
+            }
+        }
+        if(pc == yc) {
+            answer = true;
+        } else {
+            answer = false;
+        }
+
+        System.out.println(answer);
+    }
+}
