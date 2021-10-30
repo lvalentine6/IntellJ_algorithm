acd7e3a7a8ca58bb3dd528a03b770d8f31be4aa7
lvalentine6 <ysr1227@naver.com>
2021-10-30 15:58:18 +0900

    나누어 떨어지는 숫자 배열

---
+++ src/progammers/Divisor.java
@@ -0,0 +1,24 @@
+package progammers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Divisor {
+    public static void main(String[] args) {
+        int[] arr = {5, 9, 7, 10};
+        int divisor = 5;
+        Arrays.sort(arr);
+        List<Integer> list = new ArrayList<>();
+
+        for (int i=0; i< arr.length; i++) {
+            if(arr[i] % divisor == 0) {
+                list.add(arr[i]);
+            }
+        }
+        if(list.isEmpty()) {
+            list.add(-1);
+        }
+        int answer[] = list.stream().mapToInt(i -> i).toArray();
+    }
+}
