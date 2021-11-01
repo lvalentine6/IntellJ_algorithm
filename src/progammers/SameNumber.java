c5156ce8a821beb8811b711dff1e874bc24870f0
lvalentine6 <ysr1227@naver.com>
2021-11-02 02:52:06 +0900

    같은 숫자는 싫어

---
+++ src/progammers/SameNumber.java
@@ -0,0 +1,23 @@
+package progammers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SameNumber {
+    public static void main(String[] args) {
+        int[] arr = {1,1,3,3,0,1,1};
+        List<Integer> list = new ArrayList<>();
+        int temp = 10;
+
+        for (int i=0; i< arr.length; i++) {
+            if(temp != arr[i]) {
+                list.add(arr[i]);
+            }
+            temp = arr[i];
+        }
+
+        int[] answer = list.stream().mapToInt(i -> i).toArray();
+
+        System.out.println(list);
+    }
+}
