73809d38b22ee11211979c58c85c1ae4b458acb1
lvalentine6 <ysr1227@naver.com>
2021-11-05 01:53:09 +0900

    다트게임 (문자열 하나씩 찾기)

---
+++ src/progammers/DartGame.java
@@ -0,0 +1,49 @@
+package progammers;
+
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class DartGame {
+    public static void main(String[] args) {
+        String dartResult = "1D#2S*3S";
+        int answer = 0;
+        int[] score = new int[3];
+        String tempString = "";
+        int tempNum = 0;
+        int idx = 0;
+
+        for (int i = 0; i < dartResult.length(); i++) {
+            char c = dartResult.charAt(i);
+
+            if (c >= '0' && c <= '9') {
+                tempString += String.valueOf(c);
+            } else if (c == 'S' || c == 'D' || c == 'T') {
+                tempNum = Integer.parseInt(tempString);
+                if (c == 'S') {
+                    tempNum = (int) Math.pow(tempNum, 1);
+                } else if (c == 'D') {
+                    tempNum = (int) Math.pow(tempNum, 2);
+                } else {
+                    tempNum = (int) Math.pow(tempNum, 3);
+                }
+                score[idx++] = tempNum;
+                tempString = "";
+            } else {
+              if(c == '#') {
+                  score[idx-1] *= -1;
+              } else {
+                  score[idx-1] *= 2;
+                  if(idx - 2 <= 0) {
+                      score[idx - 2] *= 2;
+                  }
+              }
+            }
+        }
+
+        for (int i = 0; i < score.length; i++) {
+            answer += score[i];
+        }
+
+        System.out.println(answer);
+    }
+}
