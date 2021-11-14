937a2fa5975220c415c6b8eb8cb5f9d098320e76
lvalentine6 <ysr1227@naver.com>
2021-11-15 04:44:59 +0900

    3진법

---
+++ src/progammers/TernaryBase3.java
@@ -0,0 +1,28 @@
+package progammers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TernaryBase3 {
+    public static void main(String[] args) {
+        int n = 45;
+        int answer = 0;
+
+        // 3진법으로 변환해 리스트에 저장
+        List<Integer> list = new ArrayList<>();
+        int temp = n;
+        while(temp != 0) {
+            list.add(temp % 3);
+            temp /= 3;
+        }
+
+        // 10진법으로 변환하기 위한 지수 변수
+        int pow = list.size();
+        for (int i=0; i< list.size(); i++) {
+            pow--;
+            answer += list.get(i) * (int)Math.pow(3,pow);
+        }
+
+        System.out.println(answer);
+    }
+}
