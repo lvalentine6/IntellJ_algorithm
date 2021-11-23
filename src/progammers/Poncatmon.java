05e8d0528e86f0491e71d14d77ef25a6979cce77
lvalentine6 <ysr1227@naver.com>
2021-11-24 05:45:28 +0900

    폰켓몬

--- src/progammers/Poncatmon.java
+++ src/progammers/Poncatmon.java
@@ -7,21 +7,20 @@ public class Poncatmon {
         int[] nums = {3, 3, 3, 2, 2, 4};
         int answer = 0;
 
-        int max = nums.length / 2;
-        HashSet set = new HashSet();
+        int num = nums.length / 2;
 
+        // 중복을 제거하기 위해 해시셋에 넣기
+        HashSet set = new HashSet();
         for(int i : nums) {
             set.add(i);
         }
 
-        if(set.size() > max) {
-            answer = max;
+        // 두 값중 최소값을 리턴
+        if(set.size() > num) {
+            answer = num;
         }
         else {
             answer = set.size();
         }
-
-
-
     }
 }
