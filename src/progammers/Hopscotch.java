ca5e2614c999e7b9e8c96bac2e6d617fdc9d3ee7
lvalentine6 <ysr1227@naver.com>
2021-12-14 01:52:17 +0900

    땅따먹기_L2

--- src/progammers/Hopscotch.java
+++ src/progammers/Hopscotch.java
@@ -1,13 +1,24 @@
 package progammers;
 
+import java.util.Arrays;
+
 public class Hopscotch {
     public static void main(String[] args) {
         int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
         int answer = 0;
 
-        for (int i = 0; i < land.length; i++) {
-            
+        // DP를 이용하여 최댓값 구하기
+        for(int i=1; i<land.length; i++) {
+            land[i][0] += Math.max(land[i-1][1], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0], Math.max(land[i-1][2], land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][1], Math.max(land[i-1][0], land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0], Math.max(land[i-1][1], land[i-1][2]));
         }
 
+        int[] temp = land[land.length-1];
+        Arrays.sort(temp);
+        answer = temp[3];
+
+        System.out.println(answer);
     }
 }
