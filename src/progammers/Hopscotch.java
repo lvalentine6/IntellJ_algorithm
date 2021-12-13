dbd0438736af9ebdc5cc1e519825bb98427fa028
lvalentine6 <ysr1227@naver.com>
2021-12-13 23:44:51 +0900

    땅따먹기_L2

---
+++ src/progammers/Hopscotch.java
@@ -0,0 +1,13 @@
+package progammers;
+
+public class Hopscotch {
+    public static void main(String[] args) {
+        int[][] land = {{1,2,3,5},{5,6,7,8},{4,3,2,1}};
+        int answer = 0;
+
+        for (int i = 0; i < land.length; i++) {
+            
+        }
+
+    }
+}
