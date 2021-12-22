cbf521c9d3d01cf22a249bf74cf57a0482b85f2d
lvalentine6 <ysr1227@naver.com>
2021-12-23 05:20:02 +0900

    전화번호 목록_L2

--- src/progammers/PhoneNumber_L2.java
+++ src/progammers/PhoneNumber_L2.java
@@ -1,7 +1,27 @@
 package progammers;
 
+import java.util.HashMap;
+
 public class PhoneNumber_L2 {
     public static void main(String[] args) {
+        String[] phone_book = {"119", "97674223", "1195524421"};
+        boolean answer = true;
+
+        HashMap<String, Integer> hs = new HashMap<>();
+
+        // 전화번호 해시맵에 넣기
+        for (int i = 0; i < phone_book.length; i++) {
+            hs.put(phone_book[i], i);
+        }
+
+        // containsKey과 substring으로 접두어인지 검사
+        for (int i = 0;i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                if(hs.containsKey(phone_book[i].substring(0,j))) {
+                    answer = false;
+                }
+            }
+        }
 
     }
 }
