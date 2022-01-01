b38bae69d8c7f464123ad098c5907698c6718d6f
lvalentine6 <ysr1227@naver.com>
2022-01-02 00:48:18 +0900

    124 나라의 숫자_L2

--- src/progammers/CountryNumber124.java
+++ src/progammers/CountryNumber124.java
@@ -2,18 +2,24 @@ package progammers;
 
 public class CountryNumber124 {
     public static void main(String[] args) {
-        int n = 9;
+        int n = 10;
         String answer = "";
 
-        while(n > 0) {
-            if(n % 3 < 10) {
-                answer += n % 3;
-            }
-            else {
+        // 효율성 통과를 위한 sb (그냥 더하면 통과 못함)
+        StringBuilder sb = new StringBuilder();
 
-            }
+        // 핵심은 3진법과 같지만 0에 대한 처리가 다름
+        while(n > 0) {
+            int remain = n % 3;
             n /= 3;
+            if(remain == 0) {
+                remain = 4;
+                n--;
+            }
+            sb.append(remain);
         }
+
+        answer = sb.reverse().toString();
 
         System.out.println(answer);
     }
