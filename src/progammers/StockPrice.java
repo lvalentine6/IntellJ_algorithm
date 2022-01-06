099f804899785507cedbb7417732be46af587970
lvalentine6 <ysr1227@naver.com>
2022-01-07 04:57:41 +0900

    주식가격_L2

---
+++ src/progammers/StockPrice.java
@@ -0,0 +1,23 @@
+package progammers;
+
+import java.util.Arrays;
+
+public class StockPrice {
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 2, 3};
+        int[] answer = new int[prices.length];
+
+        // 이중 for문을 돌면서 값 비교
+       for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                // 현재 값보다 뒤 값이 커지면
+                if (prices[i] > prices[j]) {
+                    answer[i]++;
+                    break;
+                }
+                answer[i]++;
+            }
+        }
+        System.out.println(Arrays.toString(answer));
+    }
+}
