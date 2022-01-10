2eb09d1168aa3ff770d35ab6c46d2e3f775155d0
lvalentine6 <ysr1227@naver.com>
2022-01-11 06:04:27 +0900

    구명보트_L2

---
+++ src/progammers/Boat.java
@@ -0,0 +1,12 @@
+package progammers;
+
+public class Boat {
+    public static void main(String[] args) {
+        int[] people = {70, 50, 80, 50};
+        int limit = 100;
+        int answer = 0;
+
+
+
+    }
+}
