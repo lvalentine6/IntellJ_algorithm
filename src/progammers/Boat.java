226583b667fa789829130ada20dd0ac814205179
lvalentine6 <ysr1227@naver.com>
2022-01-12 02:52:58 +0900

    구명보트_L2

--- src/progammers/Boat.java
+++ src/progammers/Boat.java
@@ -1,12 +1,26 @@
 package progammers;
 
+import java.util.Arrays;
+
 public class Boat {
     public static void main(String[] args) {
         int[] people = {70, 50, 80, 50};
         int limit = 100;
         int answer = 0;
 
+        Arrays.sort(people);
+        int idx = 0;
 
+        for (int i = people.length - 1; i >= idx; i--) {
+            if(people[i] + people[idx] > limit) {
+                answer++;
+            }
+            else {
+                idx++;
+                answer++;
+            }
+        }
 
+        System.out.println(answer);
     }
 }
