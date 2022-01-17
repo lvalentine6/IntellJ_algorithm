9b43c496165022d03065412d2c7f1ae0655f6ddd
lvalentine6 <ysr1227@naver.com>
2022-01-18 06:03:43 +0900

    위장_L2

--- src/progammers/Conceal_L2.java
+++ src/progammers/Conceal_L2.java
@@ -1,11 +1,30 @@
 package progammers;
 
+import java.util.HashMap;
+
 public class Conceal_L2 {
     public static void main(String[] args) {
         String[][] clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
-        int answer = 0;
 
+        // 곱셉을 위해 1로 초기화
+        int answer = 1;
 
+        HashMap<String, Integer> map = new HashMap<>();
+
+        // 옷 종류별로 map에 넣기
+        for (int i=0; i<clothes.length; i++) {
+            map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0) + 1);
+        }
+
+        // 의상 조합 (A와 B를 조합하는 경우의 수)
+        for(String key : map.keySet()) {
+            answer *= map.get(key)+1;
+        }
+
+        // 아무것도 안입었을 경우 제거
+        answer--;
+
+        System.out.println(answer);
 
     }
 }
