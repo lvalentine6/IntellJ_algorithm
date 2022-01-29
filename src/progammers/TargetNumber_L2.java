ead5477d306d90a39578d25123132e471dab74d9
lvalentine6 <ysr1227@naver.com>
2022-01-29 23:56:41 +0900

    타겟넘버_L2

--- src/progammers/TargetNumber_L2.java
+++ src/progammers/TargetNumber_L2.java
@@ -2,6 +2,11 @@ package progammers;
 
 public class TargetNumber_L2 {
     public static void main(String[] args) {
+        int[] numbers = {1,1,1,1,1};
+        int target = 4;
+        int answer = 0;
+
+
 
     }
 }
