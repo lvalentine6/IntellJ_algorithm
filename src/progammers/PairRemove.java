508ba117b13ce247be133cd0b069e773934b31cd
lvalentine6 <ysr1227@naver.com>
2022-02-01 22:05:59 +0900

    짝지어 제거하기_L2

---
+++ src/progammers/PairRemove.java
@@ -0,0 +1,31 @@
+package progammers;
+
+import java.util.Stack;
+
+public class PairRemove {
+    public static void main(String[] args) {
+        String s = "cdcd";
+        int answer = 0;
+
+        // 캐릭터로 스택 선언
+        Stack<Character> st = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            // 스택이 비어있지 않으면서 스택의 첫번째가 char와 같으면
+            if(!st.isEmpty() && st.peek() == ch) {
+                st.pop();
+            } else {
+              st.add(ch);
+            }
+        }
+
+        // 스택이 비어 있다면 제거할수 있으므로 1 리턴
+        if (st.isEmpty()) {
+            answer = 1;
+        }
+
+        System.out.println(answer);
+
+    }
+}
