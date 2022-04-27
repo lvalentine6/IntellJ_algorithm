b52248f036eb1459a573ed4d1e5ff7ecc3525d49
lvalentine6 <ysr1227@naver.com>
2022-04-28 00:22:09 +0900

    캐시_L2

--- src/progammers/Cache_L2.java
+++ src/progammers/Cache_L2.java
@@ -7,7 +7,6 @@ public class Cache_L2 {
         int cacheSize = 0;
         String[] cities = {"Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul", "Jeju", "Pangyo", "Seoul"};
         int answer = 0;
-
         // cities 값을 소문자로 변환
         for (int i = 0; i < cities.length; i++) {
             cities[i] = cities[i].toLowerCase();
@@ -19,10 +18,12 @@ public class Cache_L2 {
             if(list.size() > cacheSize) {
                 list.removeLast();
             }
+            // Hit인경우
             if(list.contains(cities[i])) {
                 // list에 값이 있다면 삭제하고 제일 앞으로 추가
                 list.remove(list.indexOf(cities[i]));
                 answer++;
+            // Miss인경우
             } else {
                 answer+=5;
             }
