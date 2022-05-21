4d033234de41f577c948e60e2780b6461e7524a3
lvalentine6 <ysr1227@naver.com>
2022-05-21 15:56:54 +0900

    백준 1260

--- src/BOJ/B1260.java
+++ src/BOJ/B1260.java
@@ -3,7 +3,8 @@ package BOJ;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class B1260 {
@@ -11,6 +12,8 @@ public class B1260 {
     static boolean[] visit;
     static StringBuilder sb;
     static StringTokenizer st;
+    static int[][] arr;
+    static int[] s;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,31 +21,53 @@ public class B1260 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         v = Integer.parseInt(st.nextToken());
+        visit = new boolean[n + 1];
+        sb = new StringBuilder();
 
-        // dfs와 bfs의 탐색값을 저장할 배열
-        int[][] arr = new int[n + 1][n + 1];
-        for (int i = 1; i < n + 2; i++) {
+        // 입력을 배열로 저장
+        arr = new int[n + 1][n + 1];
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             // 간선이 양방향이므로
             arr[a][b] = arr[b][a] = 1;
         }
-        System.out.println(Arrays.deepToString(arr));
+        // dfs 실행
+        dfs(v);
+        sb.append("\n");
+        // visit 배열 초기화
+        visit = new boolean[n + 1];
 
+        bfs(v);
+
+        System.out.println(sb);
     }
 
-    public static void dfs(int n, int m) {
-        for (int i = 0; i < n + 1; i++) {
-            if(!visit[i]) {
-                visit[i] = true;
-                dfs(n, m);
-
+    public static void dfs(int v) {
+        visit[v] = true;
+        sb.append(v).append(" ");
+        for (int i = 1; i < n + 1; i++) {
+            if(arr[v][i] == 1 && !visit[i]) {
+                dfs(i);
             }
         }
     }
 
-    public static void bfs() {
+    public static void bfs(int v) {
+        Queue<Integer> queue = new LinkedList<>();
+        visit[v] = true;
+        queue.add(v);
 
+        while (!queue.isEmpty()) {
+            v = queue.poll();
+            sb.append(v).append(" ");
+            for (int i = 1; i < n + 1; i++) {
+                if(arr[v][i] == 1 && !visit[i]) {
+                    queue.add(i);
+                    visit[i] = true;
+                }
+            }
+        }
     }
 }
