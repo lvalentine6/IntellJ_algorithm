a9f1b712a20b5873820bb9c10ef4e32ea85f68c5
lvalentine6 <ysr1227@naver.com>
2022-05-29 08:44:27 +0900

    백준 1003

---
+++ src/BOJ/B1003.java
@@ -0,0 +1,14 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B1003 {
+    static int t;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        t = Integer.parseInt(br.readLine());
+
+    }
+}
