9a250a55cb88cc162f86e8eec419178aedb9bfdd
lvalentine6 <ysr1227@naver.com>
2022-06-01 06:32:35 +0900

    백준 5430

--- src/BOJ/B5430.java
+++ src/BOJ/B5430.java
@@ -7,54 +7,69 @@ import java.util.*;
 public class B5430 {
     public static void main(String[] args) throws Exception {
         // 수행 시간 검사
-        long start = System.currentTimeMillis();
+//        long start = System.currentTimeMillis();
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
         //입력값 배열 저장
-        for (int i = 0; i < t; i++) {
-            ArrayList<Integer> list = new ArrayList<>();
+        roof : for (int i = 0; i < t; i++) {
+            Deque<Integer> deque = new LinkedList<>();
             String[] s = br.readLine().split("");
             int size = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine(), "[|],");
+            StringTokenizer st = new StringTokenizer(br.readLine(), "[],");
 
-            // 입력값 리스트 저장
+            // 입력값 deque 저장
             for (int j = 0; j < size; j++) {
-                list.add(Integer.parseInt(st.nextToken()));
+                deque.add(Integer.parseInt(st.nextToken()));
             }
+
             // 검사
-            int cnt = 0;
-            for (String value : s) {
-                if (value.equals("R")) {
-                    if (cnt == 0) {
-                        list.sort(Collections.reverseOrder());
-                        cnt++;
+            boolean check = true;
+            for (int j = 0; j < s.length; j++) {
+                // deque의 접근 방향을 바꾼다
+                if (s[j].equals("R")) {
+                    check = !check;
+                    continue;
+                }
+                // "D"이면서 check가 true 일 때
+                if (check) {
+                    if (deque.size() == 0) {
+                        sb.append("error").append("\n");
+                        continue roof;
                     } else {
-                        Collections.sort(list);
-                        cnt = 0;
+                        deque.poll();
+                    }
+                    // check가 false 일 때
+                } else {
+                    if (deque.size() == 0) {
+                        sb.append("error").append("\n");
+                        continue roof;
+                    } else {
+                        deque.pollLast();
+                    }
+                }
+            }
+
+            // StringBuilder를 이용해서 출력문 만들기
+            sb.append("[");
+            if (deque.size() > 0) {
+                if (check) {
+                    sb.append(deque.pollFirst());
+                    while (!deque.isEmpty()) {
+                        sb.append(",").append(deque.pollFirst());
                     }
                 } else {
-                    if (list.size() == 0) {
-                        sb.append("error").append("\n");
-                    } else {
-                        list.remove(0);
+                    sb.append(deque.pollLast());
+                    while (!deque.isEmpty()) {
+                        sb.append(",").append(deque.pollLast());
                     }
                 }
             }
-            // StringBuilder를 이용해서 출력문 만들기
-            if (!(list.size() == 0)) {
-                sb.append("[");
-                for (int p : list) {
-                    sb.append(p).append(",");
-                }
-                sb.deleteCharAt(sb.lastIndexOf(","));
-                sb.append("]");
-                sb.append("\n");
-            }
+            sb.append("]").append("\n");
         }
         System.out.println(sb);
-        System.out.println((System.currentTimeMillis() - start) / 1000.0);
+//        System.out.println((System.currentTimeMillis() - start) / 1000.0);
     }
 }
