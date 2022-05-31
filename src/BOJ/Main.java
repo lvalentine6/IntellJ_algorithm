f108d19d4a234a5c149f1edace82261ac13fcc29
lvalentine6 <ysr1227@naver.com>
2022-06-01 06:49:12 +0900

    백준 5430

--- src/BOJ/Main.java
+++ src/BOJ/Main.java
@@ -1,7 +1,6 @@
 package BOJ;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
