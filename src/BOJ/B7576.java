39dbd1c1bc9559c0e32417f3a9201113b4a14631
lvalentine6 <ysr1227@naver.com>
2022-06-06 00:55:33 +0900

    백준 7576

--- src/BOJ/B7576.java
+++ src/BOJ/B7576.java
@@ -8,8 +8,8 @@ public class B7576 {
     static int m, n;
     static int[][] arr;
     static boolean[][] visited;
-    static int[] dx = {1,-1,0,0};
-    static int[] dy = {0,0,1,-1};
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
     static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws Exception {
@@ -21,68 +21,71 @@ public class B7576 {
         arr = new int[n][m];
         visited = new boolean[n][m];
 
-        // 1. 입력값 배열에 삽입
+        // 입력값 배열에 삽입
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        // 2. 배열에서 익은 토마토(1)을 발견하면 List에 배열 형태로 추가
+        // 배열에서 익은 토마토(1)을 발견하면 List에 배열 형태로 추가
         List<int[]> list = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if(arr[i][j] == 1) {
-                    list.add(new int[] {i, j});
+                if (arr[i][j] == 1) {
+                    list.add(new int[]{i, j});
                 }
             }
         }
-
-        // bfs를 실행하고 반환값중 최솟값 출력
-        answer = Math.min(bfs(list),answer);
-        System.out.println(answer);
+        // bfs 실행후 출력
+        System.out.println(bfs(list));
     }
+
     static int bfs(List<int[]> list) {
-        //
-        int[][] arr2 = arr.clone();
+        // 배열에서 탐색한 최댓값을 반환하기 위한 변수
         int max = 0;
-        boolean[][] visited2 = visited.clone();
         Queue<int[]> queue = new LinkedList<>();
+        // List에서 배열을 뽑아 queue에 삽입
+        // queue.addAll(list); 로 바꿀수 있음
         for (int i = 0; i < list.size(); i++) {
-            int[] listTemp = list.get(i);
-            queue.add(listTemp);
+            queue.add(list.get(i));
         }
+        // bfs 실행
         while (!queue.isEmpty()) {
             int[] temp = queue.poll();
             int nowX = temp[0];
             int nowY = temp[1];
-            visited2[nowX][nowY] = true;
+            visited[nowX][nowY] = true;
+
             for (int i = 0; i < 4; i++) {
                 int nextX = nowX + dx[i];
                 int nextY = nowY + dy[i];
-
-                if(nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) {
+                // 탐색 범위를 벗어나면
+                if (nextX < 0 || nextY < 0 || nextX >= n || nextY >= m) {
                     continue;
                 }
-                if (visited2[nextX][nextY] || arr2[nextX][nextY] == -1 || arr2[nextX][nextY] == 1) {
+                // 방문한 노드거나 값이 -1, 0 이면
+                if (visited[nextX][nextY] || arr[nextX][nextY] == -1 || arr[nextX][nextY] == 1) {
                     continue;
                 }
-
-                queue.add(new int[] {nextX, nextY});
-                visited2[nextX][nextY] = true;
-                arr2[nextX][nextY] = arr2[nowX][nowY] + 1;
+                queue.add(new int[]{nextX, nextY});
+                visited[nextX][nextY] = true;
+                arr[nextX][nextY] = arr[nowX][nowY] + 1;
             }
         }
-
+        // 완성된 배열을 탐색
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if(arr2[i][j] == 0) {
+                // 배열 값중 0이 있다면
+                if (arr[i][j] == 0) {
                     return -1;
                 } else {
-                    max = Math.max(arr2[i][j], max);
+                    // 탐색값중 가장 큰 값을 max에 배정
+                    max = Math.max(arr[i][j], max);
                 }
             }
         }
+        // 하루를 빼줘야 함
         return max - 1;
     }
 }
