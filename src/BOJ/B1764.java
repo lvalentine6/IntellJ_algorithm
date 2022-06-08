d2128e92cd28095fe8bdabb4ff840f6176df7115
lvalentine6 <ysr1227@naver.com>
2022-06-09 06:12:52 +0900

    백준 1764

---
+++ src/BOJ/B1764.java
@@ -0,0 +1,34 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class B1764 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        Set<String> dset = new TreeSet<>();
+        Set<String> bset = new TreeSet<>();
+        String[] d = new String[n];
+        String[] b = new String[m];
+
+        for (int i = 0; i < n; i++) {
+            dset.add(br.readLine());
+        }
+        for (int i = 0; i < m; i++) {
+            bset.add(br.readLine());
+        }
+
+        dset.retainAll(bset);
+
+        sb.append(dset.size()).append("\n");
+        for (String s : dset) {
+            sb.append(s).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
--- src/BOJ/Main.java
+++ src/BOJ/Main.java
@@ -1,8 +1,7 @@
 package BOJ;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws Exception {
