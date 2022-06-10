6b4fc94334e6fd0ea944e2b38373e24b90e63f8e
lvalentine6 <ysr1227@naver.com>
2022-06-11 02:35:09 +0900

    수식 최대화_L2

--- src/progammers/SusikMax_L2.java
+++ src/progammers/SusikMax_L2.java
@@ -1,7 +1,46 @@
 package progammers;
 
-public class SusikMax_L2 {
-    public static void main(String[] args) {
+import javax.script.ScriptException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
+public class SusikMax_L2 {
+    public static void main(String[] args) throws ScriptException {
+        String expression = "100-200*300-500+20";
+        long answer = 0;
+        StringTokenizer st = new StringTokenizer(expression, "+-*", true);
+        LinkedList<String> list = new LinkedList<>();
+        ArrayList<Integer> resultList = new ArrayList<>();
+        while (st.hasMoreTokens()) {
+            list.add(st.nextToken());
+        }
+        String[][] arr = {{"+","-","*"},{"+","*","-"},{"-","+","*"},{"-","*","+"},{"*","+","-"},{"*","-","+"}};
+        for (int i = 1; i < list.size(); i++) {
+            for (int j = 0; j < arr.length; j++) {
+                LinkedList<String> listTemp = new LinkedList<>(list);
+                for (int k = 0; k < arr[k].length; k++) {
+                    if(listTemp.get(i).equals(arr[j][k])) {
+                        String midString = mid(listTemp.get(i-1), listTemp.get(i), listTemp.get(i+1));
+                        listTemp.set(i-1, midString);
+                        listTemp.remove(i);
+                        listTemp.remove(i);
+                    }
+                }
+                resultList.add(Math.abs(Integer.parseInt(listTemp.get(0))));
+            }
+        }
+        System.out.println(resultList);
+    }
+    static String mid(String s1, String s2, String s3) {
+        int temp = 0;
+        if(s2.equals("+")) {
+            temp += Integer.parseInt(s1) + Integer.parseInt(s3);
+        } else if (s2.equals("-")) {
+            temp += Integer.parseInt(s1) - Integer.parseInt(s3);
+        } else {
+            temp += Integer.parseInt(s1) * Integer.parseInt(s3);
+        }
+        return String.valueOf(temp);
     }
 }
