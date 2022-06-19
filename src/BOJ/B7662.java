4e40191f32df5279c0f2c448717df9058204db25
lvalentine6 <ysr1227@naver.com>
2022-06-19 23:57:14 +0900

    백준 7662

---
+++ src/BOJ/B7662.java
@@ -0,0 +1,20 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B7662 {
+    static int t, k;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        t = Integer.parseInt(br.readLine());
+        k = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < k; i++) {
+            
+        }
+        
+        
+    }
+}
