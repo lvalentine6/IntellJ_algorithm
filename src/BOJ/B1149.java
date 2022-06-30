e3d735a418788a85acfee1d39f1c95ed4cac3960
lvalentine6 <ysr1227@naver.com>
2022-07-01 04:37:19 +0900

    백준 1149

--- src/BOJ/B1149.java
+++ src/BOJ/B1149.java
@@ -23,26 +23,26 @@ public class B1149 {
             arr[i][1] = Integer.parseInt(st.nextToken());
             arr[i][2] = Integer.parseInt(st.nextToken());
         }
-        dfs(0, 0, -1);
+        // 첫번쨰 집 지정
+        for (int i = 0; i < 3; i++) {
+            cal(0, i, 0);
+        }
         System.out.println(answer);
-
     }
 
-    static void dfs(int idx, int price, int preIdx) {
-        if (idx == n) {
-            answer = Math.min(answer, price);
+    // 완전탐색 메서드
+    static void cal(int idx, int preIdx, int price) {
+        int result = price;
+        result += arr[idx][preIdx];
+
+        if (idx == n-1) {
+            answer = Math.min(answer, result);
             return;
         }
-        for (int i = idx; i < n; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (!visited[i][j]) {
-                    if(j == preIdx) continue;
-                    visited[i][j] = true;
-                    price += arr[i][j];
-                    dfs(idx + 1, price, j);
-                    visited[i][j] = false;
-                    price -= arr[i][j];
-                }
+
+        for (int i = 0; i < 3; i++) {
+            if(preIdx != i) {
+                cal(idx + 1, i, result);
             }
         }
     }
