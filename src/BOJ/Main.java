4df8388bd3dbf62b997b077365a2c607931beb6b
lvalentine6 <ysr1227@naver.com>
2022-07-02 01:16:03 +0900

    백준 1149

--- src/BOJ/Main.java
+++ src/BOJ/Main.java
@@ -1,44 +1,12 @@
 package BOJ;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
-    static ArrayList<int[]> list;
-
     public static void main(String[] args) throws Exception {
 //        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = 3;
-        long k = 5;
-        int[] answer = {};
-        int[] arr = new int[n];
-        int[] out = new int[n];
-        boolean[] visited = new boolean[n];
-        list = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            arr[i] = i + 1;
-        }
-
-        perm(arr, out, visited, 0, n);
-        answer = list.get(1);
-
-        System.out.println(list);
-        System.out.println(Arrays.toString(answer));
-    }
-
-    static void perm(int[] arr, int[] out, boolean[] visited, int depth, int n) {
-        if (depth == n) {
-            list.add(out);
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                out[depth] = arr[i];
-                perm(arr, out, visited, depth + 1, n);
-                visited[i] = false;
-            }
-        }
     }
 }
