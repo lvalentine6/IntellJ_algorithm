b239c1a6284a1616ac97457c200aa7302eed4b68
lvalentine6 <ysr1227@naver.com>
2022-07-08 05:10:38 +0900

    백준 11053

--- src/BOJ/Main.java
+++ src/BOJ/Main.java
@@ -7,6 +7,6 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
