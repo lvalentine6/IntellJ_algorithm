b3ae4192be261a97b3a15e1c304d0def96754fd6
lvalentine6 <ysr1227@naver.com>
2022-07-12 01:52:47 +0900

    백준 12865

--- src/BOJ/B12865.java
+++ src/BOJ/B12865.java
@@ -2,27 +2,39 @@ package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class B12865 {
-    static int n, k;
-    static Map<Integer, Integer> map;
-    static int[] dp;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        map = new HashMap<>();
-        dp = new int[n];
-        for (int i = 0; i < n; i++) {
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] w = new int[n + 1];
+        int[] v = new int[n + 1];
+
+        // 입력값 각 배열에 삽입
+        for (int i = 1; i < n + 1; i++) {
             st = new StringTokenizer(br.readLine());
-            map.put(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(map);
+        int[][] dp = new int[n + 1][k + 1];
 
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < k + 1; j++) {
+                // 이전까지 계산했던 가치를 저장
+                dp[i][j] = dp[i - 1][j];
+                // j의 무게에서 현재 아이템의 무게를 뺐을때 남는 무게가 존재한다면
+                if (j - w[i] >= 0) {
+                    // 이전에 저장한 값, 남는 무게의 가치 + 현재 가치중 큰 값을 저장
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+                }
+            }
+        }
+
+        System.out.println(dp[n][k]);
     }
 }
