f4d0a444e383e94cd295ca22ca64d76813ea9b51
lvalentine6 <ysr1227@naver.com>
2022-07-22 20:33:27 +0900

    프로그래머스 모음사전_L2

--- src/progammers/VowelDictionary_L2.java
+++ src/progammers/VowelDictionary_L2.java
@@ -1,31 +1,38 @@
 package progammers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VowelDictionary_L2 {
     static String[] arr;
-    static int cnt;
+    static List<String> list;
     public static void main(String[] args) {
         String word = "AAAE";
         int answer = 0;
 
-        cnt = 0;
+        list = new ArrayList<>();
         arr = new String[]{"A", "E", "I", "O", "U"};
 
+        // 완전탐색
         recursion(word, "", 0);
-        answer = cnt;
-        System.out.println(answer);
 
-
-
-    }
-    static void recursion(String word, String str, int depth) {
-        if(word.equals(str)) {
-            return;
+        // list에서 word 찾기
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i).equals(word)) {
+                answer = i;
+                break;
+            }
         }
+        System.out.println(answer);
+    }
+
+    // 완전탐색 메서드
+    static void recursion(String word, String str, int depth) {
+        list.add(str);
         if(depth == 5) {
             return;
         }
         for (int i = 0; i < arr.length; i++) {
-            cnt++;
             recursion(word, str + arr[i], depth + 1);
         }
     }
