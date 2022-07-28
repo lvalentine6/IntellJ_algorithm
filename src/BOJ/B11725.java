7739e87aaae2bab8aa6d8eb9f09fa4a974d2bd2a
lvalentine6 <ysr1227@naver.com>
2022-07-29 08:34:34 +0900

    백준 11725

---
+++ src/BOJ/B11725.java
@@ -0,0 +1,16 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class B11725 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+    }
+}
