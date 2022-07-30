b2ac023f74cff4e6ffb32dcc5cc300f80aca1fba
lvalentine6 <ysr1227@naver.com>
2022-07-31 04:33:15 +0900

    백준 11725

--- src/BOJ/B11725.java
+++ src/BOJ/B11725.java
@@ -2,21 +2,58 @@ package BOJ;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class B11725 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] parent = new int[n + 1];
+        boolean[] visited = new boolean[n + 1];
 
-        for (int i = 0; i < n; i++) {
-            List<ArrayList> list = new ArrayList<>();
+        List<ArrayList<Integer>> list = new ArrayList<>();
+        for (int i = 0; i < n + 1; i++) {
+            list.add(new ArrayList());
         }
 
+        for (int i = 1; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int left = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
+
+            list.get(left).add(right);
+            list.get(right).add(left);
+        }
+
+        // BFS
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        visited[1] = true;
+        while (!queue.isEmpty()) {
+            int temp = queue.poll();
+            for (int i : list.get(temp)) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    queue.add(i);
+                    parent[i] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < parent.length; i++) {
+            if ((parent[i] != 0)) {
+                if (i == parent.length - 1) {
+                    sb.append(parent[i]);
+                } else {
+                    sb.append(parent[i]).append("\n");
+                }
+            }
+        }
+
+        System.out.println(sb.toString());
 
     }
 }
