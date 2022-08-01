ade0e614986879cd835f58788d1cc8f237be2fcc
lvalentine6 <ysr1227@naver.com>
2022-08-01 21:14:11 +0900

    프로그래머스 괄호 회전하기_L2

--- src/progammers/ParenthesisRotation_L2.java
+++ src/progammers/ParenthesisRotation_L2.java
@@ -49,58 +49,24 @@ public class ParenthesisRotation_L2 {
     // 올바른 괄호인지 확인하는 메서드
     static void isCorrect(String s) {
         Stack<Character> stack = new Stack<>();
-
-        // 괄호의 짝이 맞는지 확인
-        // 대괄호 cnt
-        int fCnt = 0;
-        // 중괄호 cnt
-        int sCnt = 0;
-        // 소괄호 cnt
-        int tCnt = 0;
-
         for (int i = 0; i < s.length(); i++) {
-            switch (s.charAt(i)) {
-                case '[':
-                    fCnt++;
-                    break;
-                case ']':
-                    fCnt--;
-                    break;
-                case '(':
-                    sCnt++;
-                    break;
-                case ')':
-                    sCnt--;
-                    break;
-                case '{':
-                    tCnt++;
-                    break;
-                case '}':
-                    tCnt--;
-                    break;
-            }
-        }
-        // 짝이 맞는다면 Stack에 넣고 짝이 맞으면 제거
-        if (fCnt == 0 && sCnt == 0 && tCnt == 0) {
-            for (int i = 0; i < s.length(); i++) {
-                if(s.charAt(i) == '[') {
-                    stack.push(s.charAt(i));
-                } else if((stack.contains('[')) && s.charAt(i) == ']') {
+            if (stack.isEmpty()) {
+                stack.push(s.charAt(i));
+            } else {
+                if ((stack.contains('[')) && s.charAt(i) == ']') {
                     stack.pop();
-                } else if(s.charAt(i) == '(') {
-                    stack.push(s.charAt(i));
                 } else if ((stack.contains('(')) && s.charAt(i) == ')') {
                     stack.pop();
-                } else if (s.charAt(i) == '{') {
-                    stack.push(s.charAt(i));
-                } else if((stack.contains('{')) && s.charAt(i) == '}') {
+                } else if ((stack.contains('{')) && s.charAt(i) == '}') {
                     stack.pop();
+                } else {
+                    stack.add(s.charAt(i));
                 }
             }
-            // Stack의 size가 0이면 조건 만족
-            if(stack.size() == 0) {
-                cnt++;
-            }
+        }
+        // Stack의 size가 0이면 조건 만족
+        if (stack.size() == 0) {
+            cnt++;
         }
     }
 }
