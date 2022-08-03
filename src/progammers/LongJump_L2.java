13505e9eca2ae7f2e46a14ddad94f194a30c4c74
lvalentine6 <ysr1227@naver.com>
2022-08-04 04:01:05 +0900

    프로그래머스 멀리 뛰기_L2

--- src/progammers/LongJump_L2.java
+++ src/progammers/LongJump_L2.java
@@ -2,10 +2,19 @@ package progammers;
 
 public class LongJump_L2 {
     public static void main(String[] args) {
-        int n = 4;
+        int n = 1;
         long answer = 0;
 
+        long[] dp = new long[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
 
+        for (int i = 3; i < n + 1; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+        }
 
+        answer = dp[n];
+
+        System.out.println(answer);
     }
 }
