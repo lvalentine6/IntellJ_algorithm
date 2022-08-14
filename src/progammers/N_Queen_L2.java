db518196b63e21dceda498df5a5dd219d187c5bf
lvalentine6 <ysr1227@naver.com>
2022-08-14 23:07:58 +0900

    프로그래머스 N-queen_L2

--- src/progammers/N_Queen_L2.java
+++ src/progammers/N_Queen_L2.java
@@ -27,7 +27,7 @@ public class N_Queen_L2 {
 
         for (int i = 0; i < n; i++) {
             arr[row] = i;
-            // 리턴값이 true이면 BackTracking
+            // 리턴값이 true이면(열을 늘려가며) BackTracking
             if(check(row)) {
                 bt(n ,row + 1);
             }
@@ -37,11 +37,10 @@ public class N_Queen_L2 {
     // 가로, 세로, 대각선 확인 메서드
     static boolean check(int row) {
         for (int i = 0; i < row; i++) {
-            // 가로, 세로 확인
+            // 0부터 row - 1까지 순환하면서 같은 행인지 검사
             if(arr[i] == arr[row]) {
                 return false;
             }
-
             // 대각선 확인 (두 점의 기울기가 같으면 대각선에 위치한 것)
             if(Math.abs(row - i) == Math.abs(arr[row] - arr[i])) {
                 return false;
