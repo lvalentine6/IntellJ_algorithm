3661d92cb9b46406e968b3a94b48a300695fde61
lvalentine6 <ysr1227@naver.com>
2022-08-16 21:43:29 +0900

    프로그래머스 하노이의탑_L2

--- src/progammers/TowerOfHanoi_L2.java
+++ src/progammers/TowerOfHanoi_L2.java
@@ -1,11 +1,20 @@
 package progammers;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class TowerOfHanoi_L2 {
     public static void main(String[] args) {
         int n = 2;
-        int[][] answer = {};
+        int[][] answer = new int[3][n];
 
+        Stack<Integer> stack = new Stack<>();
 
-        System.out.println(answer);
+        for (int i = n; i > 0; i--) {
+            stack.add(i);
+        }
+
+        System.out.println(stack);
+        System.out.println(Arrays.deepToString(answer));
     }
 }
