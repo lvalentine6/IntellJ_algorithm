3b86e0451243a7cb1010698004d499878fc66d44
lvalentine6 <ysr1227@naver.com>
2022-08-18 03:43:28 +0900

    프로그래머스 하노이의탑_L2

--- src/progammers/TowerOfHanoi_L2.java
+++ src/progammers/TowerOfHanoi_L2.java
@@ -6,34 +6,16 @@ import java.util.List;
 import java.util.Stack;
 
 public class TowerOfHanoi_L2 {
+    static List<int[]> list;
+
     public static void main(String[] args) {
         int n = 3;
-        List<int[]> list = new ArrayList<>();
-        Stack<Integer> f = new Stack<>();
-        Stack<Integer> s = new Stack<>();
-        Stack<Integer> t = new Stack<>();
+        list = new ArrayList<>();
 
-        for (int i = n; i > 0; i--) {
-            f.add(i);
-        }
+        // 재귀 호출
+        recursion(1, 2, 3, n);
 
-        while (!(f.isEmpty())) {
-            if (f.size() == 1) {
-                t.push(f.pop());
-                list.add(new int[]{1, 3});
-            } else {
-                s.push(f.pop());
-                list.add(new int[]{1, 2});
-            }
-        }
-
-        while (!(s.isEmpty())) {
-            if (s.size() > 0) {
-                t.push(s.pop());
-                list.add(new int[]{2, 3});
-            }
-        }
-
+        // 배열 출력
         int[][] answer = new int[list.size()][2];
         for (int i = 0; i < list.size(); i++) {
             int[] temp = list.get(i);
@@ -43,5 +25,18 @@ public class TowerOfHanoi_L2 {
 
         System.out.println(Arrays.deepToString(answer));
 
+    }
+
+    // 재귀 메서드
+    static void recursion(int start, int via, int end, int n) {
+        if (n == 1) {
+            list.add(new int[]{start, end});
+            return;
+        }
+        // 1번 기둥에서 3번 기둥을 거쳐 2번 기둥으로
+        recursion(start, end, via, n - 1);
+        list.add(new int[]{start, end});
+        // 2번 기둥에서 1번 기둥을 거쳐 3번 기둥으로
+        recursion(via, start, end, n - 1);
     }
 }
