aabf44d27db3533ea4433d147f3a56bc4798bbd5
lvalentine6 <ysr1227@naver.com>
2022-08-25 21:31:30 +0900

    프로그래머스 성격 유형 검사하기

--- src/progammers/CharacterCheck.java
+++ src/progammers/CharacterCheck.java
@@ -9,10 +9,14 @@ public class CharacterCheck {
         String[] survey = {"TR", "RT", "TR"};
         int[] choices = {7, 1, 3};
 
-        StringBuilder sb = new StringBuilder();
+        // 각 지표별 배열 생성
         String[] character = {"RT", "CF", "JM", "AN"};
-        List<Map<Character, Integer>> list = new ArrayList<>();
 
+        // 리스트 안에 Map 선언
+        List<Map<Character, Integer>> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        // 지표별 특성 Map 삽입
         for (int i = 1; i <= 4; i++) {
             Map<Character, Integer> subMap = new HashMap<>();
             subMap.put(character[i-1].charAt(0), 0);
@@ -20,11 +24,14 @@ public class CharacterCheck {
             list.add(subMap);
         }
 
+        // survey 요소를 돌며 점수 판별
         for (int i = 0; i < survey.length; i++) {
             char c1 = survey[i].charAt(0);
             char c2 = survey[i].charAt(1);
+            // 점수를 위한 변수
             int num = 0;
-            if(choices[i] > 0 && choices[i] < 4) {
+            // 1~3점 이면
+            if(choices[i] < 4) {
                 switch (choices[i]) {
                     case 1 : num = 3;
                     break;
@@ -33,11 +40,13 @@ public class CharacterCheck {
                     case 3 : num = 1;
                     break;
                 }
+                // 각 유형에 맞는 점수 Map에 삽입
                 for (int j = 0; j < list.size(); j++) {
                     if(list.get(j).get(c1) != null) {
                         list.get(j).put(c1, list.get(j).getOrDefault(c1, 0) + num);
                     }
                 }
+                // 5~7점 이면
             } else if(choices[i] > 4) {
                 switch (choices[i]) {
                     case 5 : num = 1;
@@ -55,18 +64,19 @@ public class CharacterCheck {
             }
         }
 
+        // Map에서 더 큰 성격 유형을 sb에 추가
+        // 같은 점수면 첫번째 값을 추가
         for (int i = 0; i < list.size(); i++) {
             Set<Character> set = list.get(i).keySet();
             Object[] ch = set.toArray();
             if(list.get(i).get(ch[0]) >= list.get(i).get(ch[1])) {
                 sb.append(ch[0]);
-            } else if (list.get(i).get(ch[0]) < list.get(i).get(ch[1])) {
+            } else{
                 sb.append(ch[1]);
             }
         }
 
         String answer = sb.toString();
-        System.out.println(list);
         System.out.println(answer);
     }
 }
