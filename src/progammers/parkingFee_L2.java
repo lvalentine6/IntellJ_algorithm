9f95a995d7041858bf3fe73e6ccb8cbfbf0e4bee
lvalentine6 <ysr1227@naver.com>
2022-08-30 01:01:50 +0900

    프로그래머스 주차 요금 계산_L2

--- src/progammers/parkingFee_L2.java
+++ src/progammers/parkingFee_L2.java
@@ -6,15 +6,19 @@ import java.util.*;
 
 public class parkingFee_L2 {
     static int[] answer;
+    static Map<String, Integer> parseMap;
     public static void main(String[] args) throws ParseException {
         int[] fees = {180, 5000, 10, 600};
         String[] records = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
 
         // 총 출입한 차량을 체크하기 위한 set
-        Set<String> set = new TreeSet<>();
+        Set<String> set = new HashSet<>();
 
         // 출입시간을 기록하기 위한 map
         Map<String, String> map = new HashMap<>();
+
+        parseMap = new TreeMap<>();
+
 
         for (int i = 0; i < records.length; i++) {
             String[] tempRecords = records[i].split(" ");
@@ -24,7 +28,8 @@ public class parkingFee_L2 {
                 map.put(tempRecords[1], tempRecords[0]);
                 // 출차라면 입차시간, 출차시간, 차량번호를 cul에 넘김
             } else {
-                cul(map.get(tempRecords[1]), tempRecords[0], tempRecords[1], fees);
+                parse(map.get(tempRecords[1]), tempRecords[0], tempRecords[1]);
+//                cul(map.get(tempRecords[1]), tempRecords[0], tempRecords[1], fees);
                 map.remove(tempRecords[1]);
             }
         }
@@ -32,28 +37,44 @@ public class parkingFee_L2 {
         // map에 남아있는 차량이 있다면
         if(!map.isEmpty()) {
             for(String s : map.keySet()) {
-                cul(map.get(s), "23:59", s, fees);
+                parse(map.get(s), "23:59", s);
                 map.remove(s);
             }
         }
 
+        // 계산 메서드 호출
+        cul(fees);
+
         answer = new int[set.size()];
+        List<Integer> list = new ArrayList<>();
+        for(String key : parseMap.keySet()) {
+            list.add(parseMap.get(key));
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        System.out.println(Arrays.toString(answer));
     }
-    static void cul(String in, String out, String carNum, int[] fees) throws ParseException {
+    static void parse(String in, String out, String carNum) throws ParseException {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
         Date outTime = simpleDateFormat.parse(out);
         Date inTime = simpleDateFormat.parse(in);
         int dif = (int) ((outTime.getTime() - inTime.getTime()) / 60000);
-        System.out.println(dif);
+        parseMap.put(carNum, parseMap.getOrDefault(carNum, 0) + dif);
+    }
 
-        int fee = 0;
-        if(dif < fees[0]) {
-            fee = fees[1];
-        } else {
-            int addTime = dif - fees[0];
-            addTime = (int) Math.ceil(addTime / fees[2]);
-            fee = addTime * fees[3] + fees[1];
+    static void cul(int[] fees) {
+        for(String key : parseMap.keySet()) {
+            if(parseMap.get(key) < fees[0]) {
+                parseMap.put(key, fees[1]);
+            } else {
+                double addTime = parseMap.get(key) - fees[0];
+                addTime = Math.ceil(addTime / fees[2]);
+                int result = (int) (addTime * fees[3] + fees[1]);
+                parseMap.put(key, result);
+            }
         }
-        System.out.println("최종 : " + fee);
     }
 }
