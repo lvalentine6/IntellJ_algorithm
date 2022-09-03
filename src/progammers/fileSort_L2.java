805ecc2430040915c18a6e31de23d9b4dd355397
lvalentine6 <ysr1227@naver.com>
2022-09-03 23:30:18 +0900

    프로그래머스 파일명 정렬_L2

---
+++ src/progammers/fileSort_L2.java
@@ -0,0 +1,13 @@
+package progammers;
+
+public class fileSort_L2 {
+    public static void main(String[] args) {
+        String[] files = {"F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"};
+        String[] answer = new String[files.length];
+
+
+
+
+
+    }
+}
