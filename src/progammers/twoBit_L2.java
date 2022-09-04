687149202b693a9308c1254f8ab44f68c02da9f4
lvalentine6 <ysr1227@naver.com>
2022-09-04 23:28:46 +0900

    프로그래머스 2개 이하로 다른 비트_L2

---
+++ src/progammers/twoBit_L2.java
@@ -0,0 +1,11 @@
+package progammers;
+
+public class twoBit_L2 {
+    public static void main(String[] args) {
+        long[] numbers = {2, 7};
+        long[] answer = new long[3];
+
+
+
+    }
+}
