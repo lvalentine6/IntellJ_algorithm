e86026aa8f31f081022012c43e9a4f1f9d21b286
lvalentine6 <ysr1227@naver.com>
2022-09-06 02:36:45 +0900

    프로그래머스 2개 이하로 다른 비트_L2

--- src/progammers/twoBit_L2.java
+++ src/progammers/twoBit_L2.java
@@ -1,11 +1,25 @@
 package progammers;
 
+import java.util.Arrays;
+
 public class twoBit_L2 {
     public static void main(String[] args) {
-        long[] numbers = {2, 7};
-        long[] answer = new long[3];
+        long[] numbers = {2, 7, 3};
+        long[] answer = new long[numbers.length];
 
+        for (int i = 0; i < numbers.length; i++) {
+            long n = numbers[i] + 1;
 
+            while (true) {
+                if(Long.bitCount(numbers[i]^n) <= 2) {
+                    answer[i] = n;
+                    break;
+                }
+                n++;
+            }
+        }
+
+        System.out.println(Arrays.toString(answer));
 
     }
 }
