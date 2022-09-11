c69a8dfa78e12ac4ba96b9bbb2c8472cd7b1d846
lvalentine6 <ysr1227@naver.com>
2022-09-11 15:35:50 +0900

    프로그래머스 점프와 순간 이동_L2

--- src/progammers/jumpAndTeleport.java
+++ src/progammers/jumpAndTeleport.java
@@ -2,16 +2,21 @@ package progammers;
 
 public class jumpAndTeleport {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 5000;
         int ans = 0;
 
-        int move = 0;
+        // dp (top-down)
+        while (n > 0) {
+            // 2로 나누어 떨어진다는것은 순간이동이 가능
+            if(n % 2 == 0) {
+                n /= 2;
+                // 아니면 점프 한칸
+            } else {
+                n--;
+                ans++;
+            }
+        }
 
-        int[] dp = new int[n];
-        dp[1] = 1;
-        dp[2] = 1;
-        dp[3] = 2;
-        dp[4] = 1;
-
+        System.out.println(ans);
     }
 }
