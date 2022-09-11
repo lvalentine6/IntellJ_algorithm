48af4d89095d1c001998db15c5a14b448aeaedcf
lvalentine6 <ysr1227@naver.com>
2022-09-11 15:05:43 +0900

    프로그래머스 점프와 순간 이동_L2

---
+++ src/progammers/jumpAndTeleport.java
@@ -0,0 +1,17 @@
+package progammers;
+
+public class jumpAndTeleport {
+    public static void main(String[] args) {
+        int n = 5;
+        int ans = 0;
+
+        int move = 0;
+
+        int[] dp = new int[n];
+        dp[1] = 1;
+        dp[2] = 1;
+        dp[3] = 2;
+        dp[4] = 1;
+
+    }
+}
