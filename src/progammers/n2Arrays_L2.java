dce8d14bf0e8e9f54cb7f06f7866a662ed6d853b
lvalentine6 <ysr1227@naver.com>
2022-09-14 05:12:59 +0900

    프로그래머스 n^2 배열 자르기_L2

--- src/progammers/n2Arrays_L2.java
+++ src/progammers/n2Arrays_L2.java
@@ -10,52 +10,19 @@ public class n2Arrays_L2 {
         long left = 7;
         long right = 14;
 
-        long leftIdx = left / n;
-        long leftRest = left % n;
-        long rightIdx = right / n;
-        long rightRest = right % n;
-        List<Integer> result = new ArrayList<>();
+        List<Long> list = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            List<Integer> list = new ArrayList<>();
-            if (i == 0) {
-                int cnt = 1;
-                for (int j = 0; j < n; j++) {
-                    list.add(cnt++);
-                }
-            } else {
-                int temp = i + 1;
-                for (int j = 0; j < temp; j++) {
-                    list.add(temp);
-                }
-                if (temp < n) {
-                    int temp2 = 1;
-                    for (int j = temp; j < n; j++) {
-                        list.add(temp + temp2++);
-                    }
-                }
-            }
-
-            if (i == leftIdx) {
-                for (int j = (int) leftRest; j < list.size(); j++) {
-                    result.add(list.get(j));
-                }
-            } else if(i > leftIdx && i < rightIdx) {
-                for (int j = 0; j < list.size(); j++) {
-                    result.add(list.get(j));
-                }
-            } else if(i == rightIdx) {
-                for (int j = 0; j <= rightRest; j++) {
-                    result.add(list.get(j));
-                }
-            }
-            System.out.println(list);
+        // Max(행, 열)이 값
+        for (long i = left; i <= right; i++) {
+            list.add(Math.max(i / n, i % n) + 1);
         }
 
-        int[] answer = new int[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = Math.toIntExact(list.get(i));
         }
+
 
         System.out.println(Arrays.toString(answer));
     }
