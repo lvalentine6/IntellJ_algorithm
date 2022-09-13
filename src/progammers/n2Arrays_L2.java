a2b7f48cac73a4a6ca0d0c69a9b187bb1788cdb8
lvalentine6 <ysr1227@naver.com>
2022-09-14 04:01:36 +0900

    프로그래머스 n^2 배열 자르기_L2

--- src/progammers/n2Arrays_L2.java
+++ src/progammers/n2Arrays_L2.java
@@ -6,38 +6,57 @@ import java.util.List;
 
 public class n2Arrays_L2 {
     public static void main(String[] args) {
-        int n = 3;
-        long left = 2;
-        long right = 5;
+        int n = 4;
+        long left = 7;
+        long right = 14;
 
-        int[][] arr = new int[n][n];
+        long leftIdx = left / n;
+        long leftRest = left % n;
+        long rightIdx = right / n;
+        long rightRest = right % n;
+        List<Integer> result = new ArrayList<>();
 
-        int idx = 1;
-        while (idx <= n) {
-            for (int i = 0; i < idx; i++) {
-                for (int j = 0; j < idx; j++) {
-                    if(arr[i][j] == 0) {
-                        arr[i][j] = idx;
+        for (int i = 0; i < n; i++) {
+            List<Integer> list = new ArrayList<>();
+            if (i == 0) {
+                int cnt = 1;
+                for (int j = 0; j < n; j++) {
+                    list.add(cnt++);
+                }
+            } else {
+                int temp = i + 1;
+                for (int j = 0; j < temp; j++) {
+                    list.add(temp);
+                }
+                if (temp < n) {
+                    int temp2 = 1;
+                    for (int j = temp; j < n; j++) {
+                        list.add(temp + temp2++);
                     }
                 }
             }
-            idx++;
-        }
 
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                list.add(arr[i][j]);
+            if (i == leftIdx) {
+                for (int j = (int) leftRest; j < list.size(); j++) {
+                    result.add(list.get(j));
+                }
+            } else if(i > leftIdx && i < rightIdx) {
+                for (int j = 0; j < list.size(); j++) {
+                    result.add(list.get(j));
+                }
+            } else if(i == rightIdx) {
+                for (int j = 0; j <= rightRest; j++) {
+                    result.add(list.get(j));
+                }
             }
+            System.out.println(list);
         }
 
-        List<Integer> sub = new ArrayList<>(list.subList((int) left, (int) right + 1));
-
-        int[] answer = new int[sub.size()];
-        for (int i = 0; i < sub.size(); i++) {
-            answer[i] = sub.get(i);
+        int[] answer = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
         }
+
         System.out.println(Arrays.toString(answer));
     }
 }
