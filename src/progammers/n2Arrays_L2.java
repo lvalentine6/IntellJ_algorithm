cedc565522b0e4d9fdd8ae02473e272a3bbda1fd
lvalentine6 <ysr1227@naver.com>
2022-09-14 23:28:54 +0900

    프로그래머스 n^2 배열 자르기_L2

--- src/progammers/n2Arrays_L2.java
+++ src/progammers/n2Arrays_L2.java
@@ -10,6 +10,7 @@ public class n2Arrays_L2 {
         long left = 7;
         long right = 14;
 
+        // 값을 저장할 list
         List<Long> list = new ArrayList<>();
 
         // Max(행, 열)이 값
@@ -17,12 +18,11 @@ public class n2Arrays_L2 {
             list.add(Math.max(i / n, i % n) + 1);
         }
 
+        // list answer 배열에 복사
         int[] answer = new int[list.size()];
-
         for (int i = 0; i < list.size(); i++) {
             answer[i] = Math.toIntExact(list.get(i));
         }
-
 
         System.out.println(Arrays.toString(answer));
     }
