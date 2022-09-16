5d357f9e98c9a27682df61a8ac85c22a130784c3
lvalentine6 <ysr1227@naver.com>
2022-09-16 22:28:21 +0900

    프로그래머스 압축 리펙터링_L2

--- src/progammers/compact_L2.java
+++ src/progammers/compact_L2.java
@@ -6,43 +6,40 @@ public class compact_L2 {
     public static void main(String[] args) {
 //        String msg = "KAKAO";
         String msg = "TOBEORNOTTOBEORTOBEORNOT";
+
+        // 사전 번호를 담을 list
         List<Integer> list = new ArrayList<>();
 
         // 사전을 담을 map 생성
         Map<String, Integer> map = new HashMap<>();
 
         // map 초기화
-        int n = 1;
         char ch = 'A';
         for (int i = 0; i < 26; i++) {
-            map.put(String.valueOf(ch++), n++);
+            map.put(String.valueOf(ch++), i + 1);
         }
 
-        int start = 27;
-        // 문자열 탐색
-        roof : for (int i = 0; i < msg.length(); i++) {
-            StringBuilder sb = new StringBuilder();
-            char w = msg.charAt(i);
-            sb.append(w);
-            int cnt = 1;
+        // 사전 번호
+        int idx = 27;
 
-            while (map.containsKey(sb.toString())) {
-                int idx = i + cnt;
-                if (idx >= msg.length()) {
-                    list.add(map.get(sb.toString()));
-                    break roof;
+        for (int i = 0; i < msg.length(); i++) {
+            String s = "";
+            s += msg.charAt(i);
+            for (int j = i + 1; j < msg.length(); j++) {
+                if (!map.containsKey(s + msg.charAt(j))) {
+                    map.put(s + msg.charAt(j), idx++);
+                    break;
+                } else {
+                    s += msg.charAt(j);
                 }
-                sb.append(msg.charAt(idx));
-                cnt++;
             }
-            map.put(sb.toString(), start++);
-            if (sb.length() > 2) {
-                i += cnt - 2;
-            }
-
-            list.add(map.get(sb.delete(sb.length() - 1, sb.length()).toString()));
+            // 인덱스 조정
+            i += s.length() - 1;
+            // 번호 리스트에 저장
+            list.add(map.get(s));
         }
 
+        System.out.println(list);
         int[] answer = new int[list.size()];
         for (int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i);
