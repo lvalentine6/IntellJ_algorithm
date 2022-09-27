c06cc37aca89c570594abf22445170fd9e37f2fb
lvalentine6 <ysr1227@naver.com>
2022-09-28 05:46:14 +0900

    프로그래머스 교점에 별 찍기_L2

--- src/progammers/makeStar_L2.java
+++ src/progammers/makeStar_L2.java
@@ -8,22 +8,28 @@ public class makeStar_L2 {
     static int[] yMinMax;
 
     public static void main(String[] args) {
-        int[][] line = {{2, -1, 4}, {-2, -1, 4}, {0, -1, 1}, {5, -8, -12}, {5, 8, 12}};
+//        int[][] line = {{2, -1, 4}, {-2, -1, 4}, {0, -1, 1}, {5, -8, -12}, {5, 8, 12}};
+        int[][] line = {{1, -1, 0}, {-2, -1, 0}, {4, -1, 0}};
 
         list = new ArrayList<>();
         xMinMax = new int[] {Integer.MAX_VALUE, Integer.MIN_VALUE};
         yMinMax = new int[] {Integer.MAX_VALUE, Integer.MIN_VALUE};
 
+        // line 배열을 순회하며 교점 구하기
         for (int i = 0; i < line.length; i++) {
             for (int j = i + 1; j < line.length; j++) {
                 makePoint(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
             }
         }
 
+        // 교점을 저장하기 위한 list
         List<String> arr = new ArrayList<>();
+
+        // x,y의 좌표의 최솟값부터 최댓값까지 순회
         for (int i = yMinMax[0]; i <= yMinMax[1]; i++) {
             StringBuilder sb = new StringBuilder();
             for (int j = xMinMax[0]; j <= xMinMax[1]; j++) {
+                // 별을 만들면서 교점의 좌표에는 *찍기
                 boolean flag = false;
                 for (int k = 0; k < list.size(); k++) {
                     int x = list.get(k)[0];
@@ -40,8 +46,11 @@ public class makeStar_L2 {
             }
             arr.add(sb.toString());
         }
+
+        // list 결과 반대로
         Collections.reverse(arr);
 
+        // answer 배열에 복사
         String[] answer = new String[arr.size()];
         for (int i = 0; i < arr.size(); i++) {
             answer[i] = arr.get(i);
@@ -50,13 +59,21 @@ public class makeStar_L2 {
         System.out.println(Arrays.toString(answer));
     }
 
+    // 교점을 구하는 메서드
     static void makePoint(long a, long b, long e, long c, long d, long f) {
+        // 분모가 0이면
+        if((a * d - b * c) == 0) {
+            return;
+        }
+
         long x = (b * f - e * d) / (a * d - b * c);
         long y = (e * c - a * f) / (a * d - b * c);
 
+        // 좌표가 정수라면
         if((b * f - e * d) % (a * d - b * c) == 0 && (e * c - a * f) % (a * d - b * c) == 0) {
             list.add(new int[] {(int)x, (int) y});
 
+            // x,y 좌표의 최댓, 최솟값 저장
             xMinMax[0] = Math.min(xMinMax[0], (int)x);
             xMinMax[1] = Math.max(xMinMax[1], (int)x);
             yMinMax[0] = Math.min(yMinMax[0], (int)y);
