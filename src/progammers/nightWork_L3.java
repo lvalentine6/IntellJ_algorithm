4a840500cd198f76c3215f9d4718e3f8d9d5d665
lvalentine6 <ysr1227@naver.com>
2022-10-05 19:42:15 +0900

    프로그래머스 야근 지수_L3

--- src/progammers/nightWork_L3.java
+++ src/progammers/nightWork_L3.java
@@ -1,32 +1,25 @@
 package progammers;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class nightWork_L3 {
     public static void main(String[] args) {
         int n = 4;
-        int[] works = {1,1};
+        int[] works = {1, 1};
         long answer = 0;
 
-        List<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < works.length; i++) {
-            list.add(works[i]);
-        }
-
         while (n > 0) {
-            int idx = list.indexOf(Collections.max(list));
-            if(list.get(idx) == 0) {
+            Arrays.sort(works);
+            int idx = works.length - 1;
+            if(works[idx] == 0) {
                 break;
             }
-            list.set(idx, list.get(idx) - 1);
+            works[idx]--;
             n--;
         }
 
-        for (int i = 0; i < list.size(); i++) {
-            answer += Math.pow(list.get(i), 2);
+        for (int i = 0; i < works.length; i++) {
+            answer += Math.pow(works[i], 2);
         }
 
         System.out.println(answer);
