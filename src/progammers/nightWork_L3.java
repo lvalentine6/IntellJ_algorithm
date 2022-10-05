6bc15ddfc72f2cd6ee2dfa7786e7f6a48d3f1d9d
lvalentine6 <ysr1227@naver.com>
2022-10-06 00:30:39 +0900

    프로그래머스 야근 지수_L3

--- src/progammers/nightWork_L3.java
+++ src/progammers/nightWork_L3.java
@@ -5,21 +5,29 @@ import java.util.*;
 public class nightWork_L3 {
     public static void main(String[] args) {
         int n = 4;
-        int[] works = {1, 1};
+        int[] works = {4, 3, 3};
         long answer = 0;
 
+        // 내림차순 우선순위 큐 선언
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+
+        // 큐에 배열값 넣기
+        for (int i = 0; i < works.length; i++) {
+            queue.add(works[i]);
+        }
+
         while (n > 0) {
-            Arrays.sort(works);
-            int idx = works.length - 1;
-            if(works[idx] == 0) {
+            // 가장 큰 수가 0이면 탐색중단
+            if (queue.peek() == 0) {
                 break;
             }
-            works[idx]--;
+            queue.add(queue.poll() - 1);
             n--;
         }
 
-        for (int i = 0; i < works.length; i++) {
-            answer += Math.pow(works[i], 2);
+        // 큐에 있는 값 제곱해서 더하기
+        while (!queue.isEmpty()) {
+            answer += Math.pow(queue.poll(), 2);
         }
 
         System.out.println(answer);
