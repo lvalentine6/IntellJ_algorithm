754ea0088b90e92966a437103cfed0504bf272af
lvalentine6 <ysr1227@naver.com>
2022-10-11 03:12:53 +0900

    프로그래머스 단어 변환_L3

--- src/progammers/wordConvert_L3.java
+++ src/progammers/wordConvert_L3.java
@@ -6,21 +6,19 @@ import java.util.List;
 
 public class wordConvert_L3 {
     static List<Integer> list;
-    static List<Integer> temp;
     public static void main(String[] args) {
         String begin = "hit";
         String target = "cog";
-        String[] words = {"hot", "dot", "dog", "lot", "log", "cog"};
+        String[] words = {"cog", "log", "lot", "dog", "dot", "hot"};
         int answer = 0;
 
         list = new ArrayList<>();
-        temp = new ArrayList<>();
 
         boolean[] visited = new boolean[words.length];
 
         boolean flag = false;
         for (int i = 0; i < words.length; i++) {
-            if(begin.equals(words[i])) {
+            if(target.equals(words[i])) {
                 flag = true;
                 break;
             }
@@ -31,14 +29,22 @@ public class wordConvert_L3 {
 
         dfs(begin, target, words, visited,0);
 
-        System.out.println(list);
+        answer = Collections.min(list);
+
+        System.out.println(answer);
     }
     static void dfs(String begin, String target, String[] word, boolean[] visited, int cnt) {
-        temp.clear();
-//        if(begin.equals(target)) {
-//            list.add(cnt);
-//            return;
-//        }
+        if(cnt != 0) {
+            int temp = 0;
+            for (int i = 0; i < word[0].length(); i++) {
+                if(begin.charAt(i) != target.charAt(i)) {
+                    temp++;
+                }
+            }
+            if(temp == 1) {
+                list.add(cnt + 1);
+            }
+        }
 
         for (int i = 0; i < word.length; i++) {
             int dif = 0;
@@ -47,25 +53,9 @@ public class wordConvert_L3 {
                     dif++;
                 }
             }
-            temp.add(dif);
-        }
-
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < temp.size(); i++) {
-            if(temp.get(i) != 0 && temp.get(i) < min) {
-                min = temp.get(i);
-            }
-        }
-        System.out.println(min);
-
-        for (int i = 0; i < temp.size(); i++) {
-            if(begin.equals(target)) {
-                list.add(cnt);
-                return;
-            }
-            if(temp.get(i) == min && !visited[i]) {
+            if(dif == 1 && !visited[i]) {
                 visited[i] = true;
-                dfs(word[i], target, word, visited,cnt + 1);
+                dfs(word[i], target, word, visited, cnt + 1);
             }
         }
     }
