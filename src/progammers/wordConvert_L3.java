120a6b4bc5c6baa5f61eacd6f00eb389fef047e9
lvalentine6 <ysr1227@naver.com>
2022-10-11 03:18:16 +0900

    프로그래머스 단어 변환_L3

--- src/progammers/wordConvert_L3.java
+++ src/progammers/wordConvert_L3.java
@@ -12,10 +12,12 @@ public class wordConvert_L3 {
         String[] words = {"cog", "log", "lot", "dog", "dot", "hot"};
         int answer = 0;
 
+        // 단계를 저장할 리스트
         list = new ArrayList<>();
-
+        // 방문 여부 배열
         boolean[] visited = new boolean[words.length];
 
+        // target이 words 있는지 검사
         boolean flag = false;
         for (int i = 0; i < words.length; i++) {
             if(target.equals(words[i])) {
@@ -23,29 +25,36 @@ public class wordConvert_L3 {
                 break;
             }
         }
+        // target이 words 배열에 없다면 0 리턴
         if(!flag) {
 //            return answer;
         }
 
+        // dfs 탐색
         dfs(begin, target, words, visited,0);
 
+        // 리스트에서 최솟값이 정답
         answer = Collections.min(list);
 
         System.out.println(answer);
     }
     static void dfs(String begin, String target, String[] word, boolean[] visited, int cnt) {
+        // 첫번째 cnt가 아니면
         if(cnt != 0) {
             int temp = 0;
+            // 현재 문자열이 target 문자열과 한글자만 다르다면
             for (int i = 0; i < word[0].length(); i++) {
                 if(begin.charAt(i) != target.charAt(i)) {
                     temp++;
                 }
             }
             if(temp == 1) {
+                // cnt값 리스트에 저장
                 list.add(cnt + 1);
             }
         }
 
+        // words 배열을 순회하며 현재 문자열과 words의 문자열 비교
         for (int i = 0; i < word.length; i++) {
             int dif = 0;
             for (int j = 0; j < word[0].length(); j++) {
@@ -53,8 +62,11 @@ public class wordConvert_L3 {
                     dif++;
                 }
             }
+            // 현재 문자열이 words 문자열과 한글자만 다르고 방문하지 않았다면
             if(dif == 1 && !visited[i]) {
+                // 방문 처리
                 visited[i] = true;
+                // 재귀 호출
                 dfs(word[i], target, word, visited, cnt + 1);
             }
         }
