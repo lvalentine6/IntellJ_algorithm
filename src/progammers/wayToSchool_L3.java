493f2f3b5ea62719f49883baac853780ad09972e
lvalentine6 <ysr1227@naver.com>
2022-10-12 22:43:49 +0900

    프로그래머스 등굣길_L3

---
+++ src/progammers/wayToSchool_L3.java
@@ -0,0 +1,11 @@
+package progammers;
+
+public class wayToSchool_L3 {
+    public static void main(String[] args) {
+        int m = 4;
+        int n = 3;
+        int[][] puddles = {{2,2}};
+
+
+    }
+}
