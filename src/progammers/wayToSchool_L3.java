4db340ba4b226a9bbde2d0b143b53cf7d7cd7505
lvalentine6 <ysr1227@naver.com>
2022-10-16 02:15:20 +0900

    프로그래머스 등굣길_L3

--- src/progammers/wayToSchool_L3.java
+++ src/progammers/wayToSchool_L3.java
@@ -6,7 +6,7 @@ public class wayToSchool_L3 {
     public static void main(String[] args) {
         int m = 4;
         int n = 3;
-        int[][] puddles = {{2,2}};
+        int[][] puddles = {{2, 2}};
         int answer = 0;
 
         int[][] arr = new int[n][m];
@@ -18,22 +18,22 @@ public class wayToSchool_L3 {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if(arr[i][j] == -1) {
+                if (arr[i][j] == -1) {
                     arr[i][j] = 0;
                     continue;
                 }
 
-                if(i != 0) {
+                if (i != 0) {
                     arr[i][j] += arr[i - 1][j] % 1000000007;
                 }
 
-                if(j != 0) {
+                if (j != 0) {
                     arr[i][j] += arr[i][j - 1] % 1000000007;
                 }
             }
         }
 
-        answer = arr[n - 1][m - 1];
+        answer = arr[n - 1][m - 1] % 1000000007;
         System.out.println(answer);
     }
 }
